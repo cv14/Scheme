b87bfe4630f864af08a9002f292025c6e729cc09
cv14 <spaudyal@gmail.com>
2018-02-17 23:57:53 -0500

    easy

--- Two-lrg.scm
+++ Two-lrg.scm
@@ -1,2 +1,7 @@
 (define (two-lrg x y z)
-	(cond((and (> x y) ( > y z)) (+ (square x) (square y)))))
+	(cond((and (> x y) ( > y z)) (+ (square x) (square y))))
+  (cond((and (> x z) ( > z y)) (+ (square x) (square z))))
+  (cond((and (> y z) ( > z x)) (+ (square y) (square z))))
+
+
+  )
